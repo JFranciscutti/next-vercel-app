1a55e6ea75fec51b0729f44774e3381ca80bd06d
Joaquin Franciscutti <JFranciscuttiEnsolvers@github.com>
2022-12-15 21:49:33 -0300

    changes in page

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Learning to deploy Next.js with Vercel</title>
         </Head>
         <Container>
           <Intro />
